65a55f9f68f1a7c405840f0ea05013cb663db329
joe <maartenjw@gmail.com>
2017-12-12 20:26:54 -0800

    Chapter 4, exercise 2

--- DBC_Chp4_2.py
+++ DBC_Chp4_2.py
@@ -1,1 +1,12 @@
-#placeholder
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okey.")
+    print("I sleep all night and work all day.")
+
+repeat_lyrics()
