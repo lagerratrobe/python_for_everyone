72870a070c7b78a14591c0fd368031fd6190b80c
Randre <randre@gmail.com>
2018-01-12 17:22:34 -0800

    Silly Ex 6.1 solution

--- ra_chp_6/ex_6_1.py
+++ ra_chp_6/ex_6_1.py
@@ -1,1 +1,15 @@
 '''Exercise 1: Write a while loop that starts at the last character in the string and works its way backwards to the first character in the string, printing each letter on a separate line, except backwards.'''
+
+word = "foo"
+
+word_length = len(word)
+transmogrifier = word_length + 1
+
+while word_length > 0:
+  index = word_length - transmogrifier
+  print(word[index])
+  word_length -= 1
+
+# Honestly though, this is retarded.  Do this instead:
+for char in word[::-1]:
+  print char
